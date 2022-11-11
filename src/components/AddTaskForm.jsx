0d428c26b113379731b90eacc170d8087afcda3d
principlebrothers <principlesbrothers@yahoo.com>
2022-11-11 23:18:16 +0000

    Add prop validation

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,18 +1,38 @@
-const AddTaskForm = ({ addTask, setNewTask, newTask }) => {
+import PropTypes from 'prop-types';
 
-  return (
-    <>
-      {/* Add Task */}
-      <div className="row">
-        <div className="col">
-          <input type="text" className="form-control form-control-lg" placeholder="Add new task" aria-label="Add new task" aria-describedby="button-addon2" value={newTask} onChange={(e) => setNewTask(e.target.value)} />
-        </div>
-        <div className="col-auto">
-          <button className="btn btn-lg btn-success" type="button" id="button-addon2" onClick={addTask}>Add Task</button>
-        </div>
+const AddTaskForm = ({ addTask, setNewTask, newTask }) => (
+  <>
+    {/* Add Task */}
+    <div className="row">
+      <div className="col">
+        <input
+          type="text"
+          className="form-control form-control-lg"
+          placeholder="Add new task"
+          aria-label="Add new task"
+          aria-describedby="button-addon2"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
       </div>
-    </>
-  );
-}
+      <div className="col-auto">
+        <button
+          className="btn btn-lg btn-success"
+          type="button"
+          id="button-addon2"
+          onClick={addTask}
+        >
+          Add Task
+        </button>
+      </div>
+    </div>
+  </>
+);
+
+AddTaskForm.propTypes = {
+  addTask: PropTypes.func.isRequired,
+  setNewTask: PropTypes.func.isRequired,
+  newTask: PropTypes.string.isRequired,
+};
 
 export default AddTaskForm;
